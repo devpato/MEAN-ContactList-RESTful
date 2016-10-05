27794ec3667e04bcaba97e4b221f69773ab8a75a
Patricio Vargas <pevargas@valdosta.edu>
2016-10-05 17:10:42 -0400

    index.html

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res){
+/*app.get('/', function(req,res){
    res.send('Hello World from server.js') 
-});
+});*/
+
+app.use(express.static(__dirname + "/public")); //create public folder
 
 app.listen(3000);
 console.log("Server running on port 3000");
