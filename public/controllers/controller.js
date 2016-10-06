87f844e5e10e0b945b5e824840090d765aec8934
Patricio Vargas <pevargas@valdosta.edu>
2016-10-06 00:13:28 -0400

    retraving data from the server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,8 +1,12 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-$http.get('/contactlist');
+$http.get('/contactlist').success(function(response){
+    console.log("I got the data i requested");//shows this message in the browser
+    $scope.contactList = response; // put the data in our html
+});
     //Can data
-    person1 = {
+    //we comment this out. this was for testing purposes
+   /* person1 = {
         name: 'Tim',
         email: 'tim@gmail.com',
         number: '(111)-222-333'
@@ -18,5 +22,5 @@ $http.get('/contactlist');
         number: '(444)-777-333'
     }
     var contactList = [person1, person2, person3];
-    $scope.contactList = contactList;
+    $scope.contactList = contactList;*/
 }]);
--- server.js
+++ server.js
@@ -9,6 +9,23 @@ app.use(express.static(__dirname + "/public")); //create public folder
 
 app.get('/contactlist', function(req,res){
     console.log("I received a GET request");
+    person1 = {
+        name: 'Tim',
+        email: 'tim@gmail.com',
+        number: '(111)-222-333'
+    }
+    person2 = {
+        name: 'Mary',
+        email: 'mary@gmail.com',
+        number: '(666)-222-999'
+    }
+    person3 = {
+        name: 'Alex',
+        email: 'alex@gmail.com',
+        number: '(444)-777-333'
+    }
+    var contactList = [person1, person2, person3];
+    res.json(contactList);//converts this to a json format
 });
 
 app.listen(3000);
