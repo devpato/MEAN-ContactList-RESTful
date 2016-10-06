c89d88081380286d96af23434124073a2651785a
Patricio Vargas <pevargas@valdosta.edu>
2016-10-05 23:52:08 -0400

    Can data finally working

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,21 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
+    //Can data
+    person1 = {
+        name: 'Tim',
+        email: 'tim@gmail.com',
+        number: '(111)-222-333'
+    }
+    person2 = {
+        name: 'Mary',
+        email: 'mary@gmail.com',
+        number: '(666)-222-999'
+    }
+    person3 = {
+        name: 'Alex',
+        email: 'alex@gmail.com',
+        number: '(444)-777-333'
+    }
+    var contactList = [person1, person2, person3];
+    $scope.contactList = contactList;
 }]);
