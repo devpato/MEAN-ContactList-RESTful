8375738b9eb72cccdc50e09edb13d96348c52960
Patricio Vargas <pevargas@valdosta.edu>
2016-10-08 15:38:54 -0400

    install body parser and mongojs

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ var app = express();
    res.send('Hello World from server.js') 
 });*/
 
-app.use(express.static(__dirname + "/public")); //create public folder
+var mongojs = require('mongojs');
+var bodyParser = require ('body-parser');
+var db = mongojs(' mongodb://pevargas:Policia9@ds049446.mlab.com:49446/meantuto',['meantuto']);
 
-app.get('/contactlist', function(req,res){
+app.use(express.static(__dirname + "/public")); //create public folder
+     
+
+/*app.get('/contactlist', function(req,res){
     console.log("I received a GET request");
     person1 = {
         name: 'Tim',
@@ -26,7 +31,7 @@ app.get('/contactlist', function(req,res){
     }
     var contactList = [person1, person2, person3];
     res.json(contactList);//converts this to a json format
-});
+});*/
 
 app.listen(3000);
 console.log("Server running on port 3000");
